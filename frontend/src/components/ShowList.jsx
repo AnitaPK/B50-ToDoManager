1af50ec053e567210fc44be7e2f8b06ce4b0a337
AnitaPK <anita.khamitkar@gmail.com>
2025-02-27 17:35:36 +0530

    update corredt code

--- frontend/src/components/ShowList.jsx
+++ frontend/src/components/ShowList.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const ShowList = () => {
   const [lists, setLists] = useState([]);
+
+  let [upadatedName, setupdatedName] = useState();
+  let [upadatedDiscri, setupdatedDiscri] = useState();
+
+  const [selectedItem, setSelectedItem] = useState(null);
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:7000/getAllItems");
@@ -15,54 +20,41 @@ const ShowList = () => {
 
   console.log(lists);
 
-
-  const [item, setItemName] = useState();
-  const [discription, setDiscription] = useState();
-
- async function handleSubmit(event) {
-    event.preventDefault();
-    const formData = {itemName:item, itemDiscription:discription};
-    const response = await axios.post("http://localhost:7000/addNewItem", formData);
+  async function handleDelete(id) {
+    const response = await axios.delete(
+      `http://localhost:7000/deleteItem/${id}`
+    );
     console.log(response.data);
-    alert(response.data.msg);
-    fetchAPI();
+    if (response.data.success) {
+      alert(response.data.msg);
+      fetchAPI();
+    } else {
+      alert(response.data.msg);
+    }
   }
 
-async function handleDelete(id){
-    const response = await axios.delete(`http://localhost:7000/deleteItem/${id}`)
-    console.log(response.data)
-    if(response.data.success){
-      alert(response.data.msg)
-      fetchAPI();
-    }else{
-      alert(response.data.msg)
-    }
+async  function handleUpdate(id){
+    const updatedFormData = {itemName:upadatedName, itemDiscription:upadatedDiscri}
+console.log(updatedFormData);
+const response = await axios.put(`http://localhost:7000/updateItem/${id}`, updatedFormData)
+if(response.data.success){
+  setupdatedName( upadatedName = '')
+  setupdatedDiscri(upadatedDiscri = '')
 }
+  }
+ function handleUpdatedEdit(item){
+console.log(item);
+setSelectedItem(item);      //update code
+setupdatedName(item.itemName);
+setupdatedDiscri(item.itemDiscription);
+ }
+
 
   return (
     <>
-
-<div>
-        <form onSubmit={handleSubmit}>
-          <div class="mb-3">
-            <label for="taskName" class="form-label">
-              Task Name
-            </label>
-            <input type="text" class="form-control" id="taskName"  onChange={(e)=>setItemName(e.target.value)}/>
-          </div>
-          <div class="mb-3">
-            <label for="taskDisri" class="form-label">
-              Task discription
-            </label>
-            <input type="text" class="form-control" id="taskDisri" onChange={(e)=>setDiscription(e.target.value)}/>
-          </div>
-
-          <button type="submit" class="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
-      <hr></hr><hr></hr>
+      <div></div>
+      <hr></hr>
+      <hr></hr>
       <table class="table">
         <thead>
           <tr>
@@ -75,19 +67,83 @@ async function handleDelete(id){
         <tbody>
           {lists.length > 0 ? (
             <>
-              
               {lists.map((item, index) => (
-                <tr key={index}>
-                  <th scope="row">{index+1}</th>
-                  <td>{item.itemName}</td>
-                  <td>{item.itemDiscription}</td>
-                  <td>
-                    <button>Edit</button>
-                    <button onClick={()=>handleDelete(item._id)}>Delete</button>
-                  </td>
-                </tr>
+                <>
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{item.itemName}</td>
+                    <td>{item.itemDiscription}</td>
+                    <td>
+                      <button
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                        onClick={() => handleUpdatedEdit(item)}
+                      >
+                        Edit
+                      </button>
+                      <button onClick={() => handleDelete(item._id)}>
+                        Delete
+                      </button>
+                    </td>
+
+                  <div key={index}
+                    class="modal fade"
+                    id="exampleModal"
+                    tabindex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="exampleModalLabel">
+                            Edit Task  {selectedItem?.itemName}
+                          </h1>
+                          <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                          ></button>
+                        </div>
+                        <div class="modal-body">
+                          <input
+                            type="text"
+                            class="form-control"
+                            value={upadatedName}
+                            onChange={(e)=>setupdatedName(e.target.value)}
+                          />
+                          <br></br>
+                          <input
+                            type="text"
+                            class="form-control"
+                            value={upadatedDiscri}
+                            onChange={(e)=>setupdatedDiscri(e.target.value)}
+                          />
+                        </div>
+                        <div class="modal-footer">
+                          <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                          >
+                            Close
+                          </button>
+                          <button
+                            type="button"
+                            class="btn btn-primary"
+                            onClick={()=>handleUpdate(item._id)}
+                          >
+                            Update changes
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </tr>
+
+                </>
               ))}
-              
             </>
           ) : (
             <p>NO Data</p>
